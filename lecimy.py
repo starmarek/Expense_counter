803b9eb715454ef5b7a1fdf9e5bd47155beeb7fa
Aleksander <apucher13@gmail.com>
2018-12-04 23:13:41 +0100

    !! SNAPSHOT 0.3 !! - Raczkujacy system komunikacji z uzytkownikiem. Nadal nie wszystkie wydatki sa posegregowane. Wybrane wydatki udaje sie juz podliczyc.

--- lecimy.py
+++ lecimy.py
@@ -35,7 +35,7 @@ for count, i in enumerate(r_handle, 1):  # loop that is reading every line in a 
         nmainy.clear()
     elif w(i, "Tytu") or w(i, "Wpłatomat") or w(i, "Operacja") or w(i, "Odbiorca") or w(i, "Nadawca") or w(i,
                                                                                                            "2018") or w(
-            i, "Prowadzonego") or w(i, "Prowadzony") or w(i, ", POL"):
+        i, "Prowadzonego") or w(i, "Prowadzony") or w(i, ", POL"):
         if count == 1:
             nmainy.append(str(i))
             mainy.append(nmainy.copy())
@@ -55,25 +55,25 @@ mieszkanie = []
 pseudowydatki = []
 pseudoprzychody = []
 spozywcze = []
-x = ["biedro", "pss", "zabka", "politechnika", "t and j", "lidl", "mihiderka", "ledi", "karni", "carrefour", "fasolka", ]
+x = ["biedro", "pss", "zabka", "politechnika", "t and j", "lidl", "mihiderka", "ledi", "karni", "carrefour",
+     "fasolka", ]
 
 for ind in mainy:
     if any("xxxx" in s for s in ind):  # KARTA
         for z in x:
             if any(z in s.lower() for s in ind):
                 spozywcze.append(ind)
-        if any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any("Lorek" in s for s in ind):
-            zw_dlugi.append(ind)
         karta.append(ind)
-    else:   # PRZELEWY
+    else:  # PRZELEWY
         przelewy.append(ind)
         if any("przychodzący" in s for s in ind) or any("Wpłatomat" in s for s in ind):  # PRZYCHODZACE
             przychody.append(ind)
             if any("Wpłatomat" in s for s in ind):
                 wplatomat.append(ind)
-            elif any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any("Lorek" in s for s in ind):
+            elif any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any(
+                    "Lorek" in s for s in ind):
                 zw_dlugi.append(ind)
-        else:    # WYCHODZACE
+        else:  # WYCHODZACE
             if any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any("Lorek" in s for s in ind):
                 dlugi.append(ind)
             elif any("mzuri" in s for s in ind):
@@ -81,9 +81,57 @@ for ind in mainy:
             elif any("Wyciąg" in s for s in ind):
                 continue
             wydatki.append(ind)
-print(spozywcze)
 
-print(str(neg) + "\n" + str(pos))
+
+
+# /////////////////////////////CZESC INTERAKTYWNA\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+
+expenses = [
+    "Artykuly spozywcze", "Czynsz za mieszkanie", "Zwroty dlugow",
+    "Moje dlugi"
+]
+
+
+for a, b in enumerate(expenses, 1):
+    print('{} {}'.format(a, b))
+
+
+wybor = int(input("Prosze podac numer informacji do wyswietlenia: "))
+
+
+switcher = {
+    1: spozywcze,
+    2: mieszkanie,
+    3: zw_dlugi,
+    4: dlugi
+}
+
+
+def oblicz():
+    wynik = 0
+    if switcher.get(wybor) == spozywcze:
+        for i in spozywcze:
+            wynik += float(i[2])
+    elif switcher.get(wybor) == mieszkanie:
+        for i in mieszkanie:
+            wynik += float(i[3])
+    elif switcher.get(wybor) == zw_dlugi:
+        for i in zw_dlugi:
+            wynik += float(i[3])
+    elif switcher.get(wybor) == dlugi:
+        for i in dlugi:
+            wynik += float(i[3])
+    print(wynik)
+
+
+def wyswietl():
+    print(switcher.get(wybor , "lipa"))
+
+
+wyswietl()
+oblicz()
+
 
 r_handle.close()
 w_handle.close()
