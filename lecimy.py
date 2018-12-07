14e73ef79d7ec4131110ee4524e14a6a2dc2a4f4
Aleksander <apucher13@gmail.com>
2018-12-07 18:41:28 +0100

    !!! SNAPSHOT 0.4 !!! Rethoughted the idea of what program should look like in versiom 1.0 - new file Guidelines. Program can now write to file every kind of expense, whether it is C.C or transfer (with text align, which code solution must be rethought)

--- lecimy.py
+++ lecimy.py
@@ -1,137 +1,201 @@
-r_handle = open("Wyciag.txt", "r")  # reading file handle
-w_handle = open("Summary.txt", 'w')  # writing file handle
+r_handle = open("Wyciag.txt", "r")
+w_handle = open("Summary.txt", 'w')
 
 
-# searches for expression in given line
 def w(it, expr):
-    b = not (it.find(expr) == -1)
-    # if expr is coma, it change it in purpose of comparing similar lines [very needed step]
     if expr == ",":
         it = it.strip("\n")
         it = it.replace(",", "")
         it = it.replace("-", "")
         return it.replace(" ", "").isdigit()
-    else:
-        return b
+    return not(it.find(expr) == -1)
 
 
-neg = 0
-pos = 0
 mainy = []
 nmainy = []
-for count, i in enumerate(r_handle, 1):  # loop that is reading every line in a file
-    # in the next line, condition that decides about usefulness of text line
+slowa_klucz = [
+    "Tytu",
+    "Wpłatomat",
+    "Operacja",
+    "Odbiorca",
+    "Nadawca",
+    "2018",
+    "Prowadzonego",
+    "Prowadzony",
+    ", POL"
+]
+
+for i in r_handle:
+
     if w(i, ","):
         i = i.split(" ")
         i = float(i.pop(0).replace(",", "."))
-        if i < 0:
-            neg += i
-        else:
-            pos += i
-        w_handle.write(str(i))
-        w_handle.write("\n\n")
         nmainy.append(str(i))
         mainy.append(nmainy.copy())
         nmainy.clear()
-    elif w(i, "Tytu") or w(i, "Wpłatomat") or w(i, "Operacja") or w(i, "Odbiorca") or w(i, "Nadawca") or w(i,
-                                                                                                           "2018") or w(
-        i, "Prowadzonego") or w(i, "Prowadzony") or w(i, ", POL"):
-        if count == 1:
-            nmainy.append(str(i))
-            mainy.append(nmainy.copy())
-            nmainy.clear()
-            continue
-        w_handle.write(i)
+
+    elif any(s in i for s in slowa_klucz) and not("Wyciąg" in i):
         nmainy.append(str(i))
 
-przelewy = []
-karta = []
-przychody = []
-wydatki = []
-wplatomat = []
-zw_dlugi = []
-dlugi = []
-mieszkanie = []
-pseudowydatki = []
-pseudoprzychody = []
-spozywcze = []
-x = ["biedro", "pss", "zabka", "politechnika", "t and j", "lidl", "mihiderka", "ledi", "karni", "carrefour",
-     "fasolka", ]
+wyrazy_wymiana = []
+wyrazy_mat = []
+wyrazy_komunikacja = []
+wyrazy_skarbonka = []
+wyrazy_zw_dlugi = []
+wyrazy_dlugi = []
+wyrazy_mieszkanie = [
+    "mzuri"
+]
+wyrazy_spozywcze = [
+    "biedro",
+    "pss",
+    "zabka",
+    "politechnika",
+    "t and j",
+    "lidl",
+    "mihiderka",
+    "ledi",
+    "karni",
+    "carrefour",
+    "fasolka"
+]
 
-for ind in mainy:
-    if any("xxxx" in s for s in ind):  # KARTA
-        for z in x:
-            if any(z in s.lower() for s in ind):
-                spozywcze.append(ind)
-        karta.append(ind)
-    else:  # PRZELEWY
-        przelewy.append(ind)
-        if any("przychodzący" in s for s in ind) or any("Wpłatomat" in s for s in ind):  # PRZYCHODZACE
-            przychody.append(ind)
-            if any("Wpłatomat" in s for s in ind):
-                wplatomat.append(ind)
-            elif any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any(
-                    "Lorek" in s for s in ind):
-                zw_dlugi.append(ind)
-        else:  # WYCHODZACE
-            if any("RACHWALIK" in s for s in ind) or any("ZUZANNA" in s for s in ind) or any("Lorek" in s for s in ind):
-                dlugi.append(ind)
-            elif any("mzuri" in s for s in ind):
-                mieszkanie.append(ind)
-            elif any("Wyciąg" in s for s in ind):
+zbior_wyrazow = [
+    wyrazy_spozywcze,
+    wyrazy_mieszkanie,
+    wyrazy_wymiana,
+    wyrazy_mat,
+    wyrazy_komunikacja,
+    wyrazy_skarbonka,
+    wyrazy_zw_dlugi,
+    wyrazy_dlugi
+]
+tmp = []
+liczba_wydatkow = 2
+i = 0
+suma = 0
+count = 0
+
+
+def take_second(elem):
+    return elem[1]
+
+
+def check(str):
+    if 'xxxx' in str[0]:
+        return 0
+    else:
+        elo = str[3].strip("\n")
+        elo = elo.replace(".", "")
+        elo = elo.replace("-", "")
+        if elo.replace(" ", "").isdigit():
+            return 1
+        else:
+            return 2
+
+
+while i < liczba_wydatkow:
+
+    for ind in mainy:
+        for s in ind:
+            if any(z in s.lower() for z in zbior_wyrazow[i]):
+                pomoc = check(ind)
+                tmp.append(ind)
+                suma += float(ind[2+pomoc])
+
+    tmp.sort(key=take_second)
+
+    for s in tmp:
+        pomoc = check(s)
+        count += 1
+        if count < 10:
+            if float(s[2+pomoc]) > -10:
+                if s[2][::-1].find('.') == 2:
+                    w_handle.write("{}     {}    {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 1 -2.32
+                    continue
+                w_handle.write("{}     {}    {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 1 -2.3
                 continue
-            wydatki.append(ind)
+            if s[2][::-1].find('.') == 2:
+                w_handle.write("{}     {}   {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 1 -12.32
+                continue
+            w_handle.write("{}     {}    {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 1 -12.3
+            continue
 
+        if float(s[2+pomoc]) > -10:
+            if s[2][::-1].find('.') == 2:
+                w_handle.write("{}    {}    {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 11 -2.32
+                continue
+            w_handle.write("{}    {}     {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 11 -2.3
+            continue
+        if s[2][::-1].find('.') == 2:
+            w_handle.write("{}    {}   {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 11 -12.32
+            continue
+        w_handle.write("{}    {}    {}    {}".format(count, s[2 + pomoc], s[0][0:10], s[1]))  # 11 -12.3
+        continue
+
+    for d in range(0, 100): w_handle.write('-')
+    w_handle.write('\n')
+    i += 1
+    count = 0
+    tmp.clear()
+
+
+r_handle.close()
+w_handle.close()
 
 
 # /////////////////////////////CZESC INTERAKTYWNA\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 
 
-expenses = [
-    "Artykuly spozywcze", "Czynsz za mieszkanie", "Zwroty dlugow",
-    "Moje dlugi"
-]
-
-
-for a, b in enumerate(expenses, 1):
-    print('{} {}'.format(a, b))
-
-
-wybor = int(input("Prosze podac numer informacji do wyswietlenia: "))
-
-
-switcher = {
-    1: spozywcze,
-    2: mieszkanie,
-    3: zw_dlugi,
-    4: dlugi
-}
-
-
-def oblicz():
-    wynik = 0
-    if switcher.get(wybor) == spozywcze:
-        for i in spozywcze:
-            wynik += float(i[2])
-    elif switcher.get(wybor) == mieszkanie:
-        for i in mieszkanie:
-            wynik += float(i[3])
-    elif switcher.get(wybor) == zw_dlugi:
-        for i in zw_dlugi:
-            wynik += float(i[3])
-    elif switcher.get(wybor) == dlugi:
-        for i in dlugi:
-            wynik += float(i[3])
-    print(wynik)
-
-
-def wyswietl():
-    print(switcher.get(wybor , "lipa"))
-
-
-wyswietl()
-oblicz()
-
-
-r_handle.close()
-w_handle.close()
+# expenses = [
+#     "Artykuly spozywcze", "Czynsz za mieszkanie", "Zwroty dlugow",
+#     "Moje dlugi"
+# ]
+#
+#
+# switcher = {
+#     1: spozywcze,
+#     2: mieszkanie,
+#     3: zw_dlugi,
+#     4: dlugi
+# }
+#
+#
+# def oblicz():
+#     wynik = 0
+#     nazwy = []
+#     liczby = []
+#     daty = []
+#     if switcher.get(wybor) == spozywcze:
+#         for i in spozywcze:
+#             wynik += float(i[2])
+#             nazwy.append(i[1])
+#             liczby.append(i[2])
+#             daty.append(i[0][0:10])
+#     elif switcher.get(wybor) == mieszkanie:
+#         for i in mieszkanie:
+#             wynik += float(i[3])
+#     elif switcher.get(wybor) == zw_dlugi:
+#         for i in zw_dlugi:
+#             wynik += float(i[3])
+#     elif switcher.get(wybor) == dlugi:
+#         for i in dlugi:
+#             wynik += float(i[3])
+#     for l,s in enumerate(nazwy, 1):
+#         print('{}   {}     {}    {}'.format(l, liczby[l-1], daty[l-1], s.replace("\n", "")))
+#     print("\nSumaryczna ilosc pieniedzy: " + str(wynik) + "\n")
+#
+#
+# def wyswietl():
+#
+#     print(switcher.get(wybor , "lipa"))
+#
+#
+# while True:
+#     print("--------------------------")
+#     for a, b in enumerate(expenses, 1):
+#         print('{} {}'.format(a, b))
+#     wybor = int(input("\nProsze podac numer informacji do wyswietlenia: "))
+#     if wybor == 99:
+#         break
+#     oblicz()
